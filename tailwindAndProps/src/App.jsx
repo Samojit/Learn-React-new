70886c83da5fd883ecfbefe3dae65aecedb161ed
Samojit Paul <paulsamo1234@gmail.com>
2024-04-10 15:28:53 +0530

    Adding Tailwind and learning few details about props

---
+++ tailwindAndProps/src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+
+  // let myObj = {
+  //   "title": "Hello",
+  //   "btnText": "Click here"
+  // }
+
+  return (
+    <>
+     <h1 className='bg-blue-600 text-black rounded-xl p-5 mb-2'>Tailwind</h1>
+     <Card userName="Samojit" btnText="Click There"/>
+     <Card userName = "Samo"/>
+    </>
+  )
+}
+
+export default App
